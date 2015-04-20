70bc1cee4ee2b51f6bbca5c75b9b72c4d5a0f00a
yiyiarrow <yiyiarrow@163.com>
2015-04-20 21:54:23 +0800

    add more page and link

---
+++ CrossinPython/version_3/webCode3.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import web
+
+urls = (
+    '/', 'index',
+    '/movie/(\d+)', 'movie',
+)
+
+render = web.template.render('templates/')
+
+db = web.database(dbn='sqlite', db='MovieSite.db')
+
+class index(object):
+    def GET(self):
+        movies = db.select('movie') 
+        return render.index(movies)
+
+class movie(object):
+    def GET(self, movie_id):
+        movie_id = int(movie_id)
+        movie = db.select('movie', where = 'id=$movie_id', vars=locals())[0]
+        return render.movie(movie)
+
+if __name__ == '__main__':
+    app = web.application(urls, globals())
+    app.run()
+
+
