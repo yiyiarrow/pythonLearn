c16aea26bb8fdff48ffc220fc03781eb80993334
yiyiarrow <yiyiarrow@163.com>
2015-05-03 21:40:32 +0800

    version_4:add serch feature

---
+++ CrossinPython/version_4/movie_table.sql
@@ -0,0 +1,5 @@
+create table movie (id, title, year, country, abstract);
+
+insert into movie values (1, 'Forrest Gump', 1994, 'USA', 'Life is a box of chocolate');
+insert into movie values (2, 'Titanic', 1997, 'USA', 'Jack and Rose: you jump, I jump');
+insert into movie values (3, 'The Shawshank Redemption', 1994, 'USA', 'Hope is a good thing');
